db482af0521b5c187f85d667a39854f9fc890324
Hoyang Tsai <hoyang.t@gmail.com>
2021-08-31 16:57:19 +0800

    docking azure

--- src/index.js
+++ src/index.js
@@ -1,51 +1,7 @@
-const path = require('path');
-const FileType = require('file-type');
-const imgur = require('imgur');
-const fs = require('fs-extra');
-const { execSync } = require('child_process');
-const hasha = require('hasha');
+const { router, line } = require('bottender/router');
 
-imgur.setClientId('7c7c95472cb8e01');
+const handleLineMessage = require('./line/message.js');
 
-module.exports = async function App(context) {
-  if (context.event.isImage) {
-    const buffer = await context.getMessageContent();
-    const { ext } = await FileType.fromBuffer(buffer);
-
-    const md5 = await hasha(buffer, { algorithm: 'md5' });
-
-    const filename = `${md5}.${ext}`;
-    const filepath = path.join(__dirname, `${filename}`);
-
-    // if (fs.existsSync(filepath)) {
-    //   await context.sendText(`圖片已傳過 🧐`);
-    // } else {
-    fs.writeFileSync(filepath, buffer);
-
-    const convertFile = path.basename(filepath, `.${ext}`) + `_conv.${ext}`;
-    execSync(
-      `convert ${filename} -morphology Edge Octagon -negate -threshold 80% ${convertFile}`,
-      {
-        cwd: __dirname,
-      }
-    );
-
-    imgur
-      .uploadFile(path.join(__dirname, convertFile))
-      .then(async (json) => {
-        // console.log('json =>', json);
-        const { link } = json;
-        await context.sendImage({
-          originalContentUrl: link,
-          previewImageUrl: link,
-        });
-      })
-      .catch((err) => {
-        console.error('imgur:', err.message);
-      });
-    // }
-  }
-  // else {
-  //   await context.sendText(`請傳送一張圖片。`);
-  // }
+module.exports = async function App() {
+  return router([line.message(handleLineMessage)]);
 };
---
+++ src/line/message.js
@@ -0,0 +1,139 @@
+const path = require('path');
+const FileType = require('file-type');
+const fs = require('fs-extra');
+const { execSync } = require('child_process');
+const hasha = require('hasha');
+
+const { BlobServiceClient } = require('@azure/storage-blob');
+
+const root = path.resolve(__dirname, '../..');
+const convertPath = path.join(root, 'converts');
+
+module.exports = async function (context) {
+  // console.log('message:\n\t', context.event.text);
+
+  const userProfile = await context.getUserProfile();
+  console.log('userProfile:\n', userProfile);
+  const { userId = '', displayName = '' } = userProfile;
+
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const dd = String(today.getDate()).padStart(2, '0');
+  const time = today.toTimeString().split(' ')[0];
+  const fullDate = `${yyyy}/${mm}/${dd}`;
+
+  const AZURE_STORAGE_CONNECTION_STRING =
+    process.env.AZURE_STORAGE_CONNECTION_STRING || '';
+
+  const blobServiceClient = BlobServiceClient.fromConnectionString(
+    AZURE_STORAGE_CONNECTION_STRING
+  );
+
+  // Create a unique name for the container
+  let containerName = 'unknown';
+  if (userId) {
+    containerName = `line-${userId}`.toLowerCase();
+  }
+
+  console.log('\nCreating container...');
+  console.log('\t', containerName);
+
+  // Get a reference to a container
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+
+  // Create the container
+  if (!(await containerClient.exists())) {
+    const createContainerResponse = await containerClient.create({
+      access: 'container',
+    });
+    console.log(`Container was created successfully.
+      requestId: ${createContainerResponse.requestId}`);
+  }
+
+  // for await (const item of containerClient.listBlobsByHierarchy('/', {
+  //   prefix: `${fullDate.replace(/\//g, '-')}`,
+  // })) {
+  //   if (item.kind === 'prefix') {
+  //     console.log(`\tBlobPrefix: ${item.name}`);
+  //   } else {
+  //     console.log(
+  //       `\tBlobItem: name - ${item.name}, last modified - ${item.properties.lastModified}`
+  //     );
+  //   }
+  //   console.log(`item: ${JSON.stringify(item, null, 2)}`);
+  // }
+
+  let uploadCounter = 1;
+  for await (const blob of containerClient.listBlobsFlat({
+    prefix: `${fullDate.replace(/\//g, '-')}`,
+  })) {
+    console.log(`Blob: ${blob.name}`);
+    if (blob.name.includes('_conv')) {
+      uploadCounter++;
+    }
+  }
+
+  if (context.event.isImage) {
+    const buffer = await context.getMessageContent();
+    const { ext } = await FileType.fromBuffer(buffer);
+
+    const md5 = await hasha(buffer, { algorithm: 'md5' });
+
+    const origBlobClient = containerClient.getBlockBlobClient(
+      `${fullDate.replace(/\//g, '-')}/${md5}.${ext}`
+    );
+    // async upload original file
+    const uploadOrigBlobResponse = origBlobClient.upload(
+      buffer,
+      Buffer.byteLength(buffer)
+    );
+
+    if (uploadOrigBlobResponse && uploadOrigBlobResponse.requestId) {
+      console.log(`Original blob uploaded successfully. 
+        responseId: ${uploadOrigBlobResponse.requestId}`);
+    }
+
+    const tempFile = path.join(convertPath, `temp.${ext}`);
+    fs.outputFileSync(tempFile, buffer);
+
+    const imageBanner = `${fullDate} ${time} ${displayName} 傳送 ${uploadCounter} 張照片`;
+
+    console.log(`imageBanner:\n\t${imageBanner}`);
+
+    const convertFile = `${md5}_conv.${ext}`;
+    execSync(
+      `convert ${tempFile} -morphology Edge Octagon -negate -threshold 80% -font "./fonts/NotoSansTC-Regular.otf" -annotate +10+20 "${imageBanner}" ${convertFile}`,
+      {
+        cwd: convertPath,
+      }
+    );
+
+    const convFilePath = path.join(convertPath, `${convertFile}`);
+    const convFileContent = fs.readFileSync(convFilePath);
+    const convFileStat = fs.statSync(convFilePath);
+
+    const convBlobClient = containerClient.getBlockBlobClient(
+      `${fullDate.replace(/\//g, '-')}/${convertFile}`
+    );
+    const uploadConvBlobResponse = await convBlobClient.upload(
+      convFileContent,
+      convFileStat.size
+    );
+
+    if (uploadConvBlobResponse && uploadConvBlobResponse.requestId) {
+      console.log(`Convert blob uploaded successfully. 
+        responseId: ${uploadConvBlobResponse.requestId}`);
+
+      const convBlobUrl = convBlobClient.url;
+      await context.sendImage({
+        originalContentUrl: convBlobUrl,
+        previewImageUrl: convBlobUrl,
+      });
+
+      fs.remove(convFilePath);
+    }
+  } else {
+    await context.sendText(`請傳送一張圖片。`);
+  }
+};
