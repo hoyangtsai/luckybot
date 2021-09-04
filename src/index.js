21965838e7362ac854b67d69a0a4957a0ba9f36a
Hoyang Tsai <hoyang.t@gmail.com>
2021-09-05 01:34:42 +0800

    Fix upload async

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const { router, line } = require('bottender/router');
 const handleLineMessage = require('./line/message.js');
 
 module.exports = async function App() {
-  return router([line.message(handleLineMessage)]);
+  return router([line.message(await handleLineMessage)]);
 };
